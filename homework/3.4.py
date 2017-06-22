2c6fcbc3936f4e67ba74f6fdfd697a357e1e8c2d
Mih-Shy <mihail-shymaev@list.ru>
2017-06-22 15:10:31 +0800

    complite

--- homework/3.4.py
+++ homework/3.4.py
@@ -1,7 +1,7 @@
-access_template = ['switchport mode access',
-                   'switchport access vlan {}',
-                   'switchport nonegotiate',
-                   'spanning-tree portfast',
-                   'spanning-tree bpduguard enable']
+command1 ="swithport trunk allowd vlan 1,3,10,20,30,100"
+command2 ="swithport trunk allowd vlan 1,3,100,200,300"
 
-print('\n'.join(access_template).format( 5 ))
+command2 = set(command2.split()[-1].split(','))
+command1 = set(command1.split()[-1].split(','))
+uniq =list(command1.intersection(command2))
+print(uniq)
